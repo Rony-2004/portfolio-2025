9cde49d792ab6017e554e30462440c075c196f04
MU AHMED <145216818+Rony-2004@users.noreply.github.com>
2025-04-26 14:39:41 +0530

    Update ProjectsMain.jsx

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -1,92 +1,64 @@
-import ProjectsText from "./ProjectsText";
 import SingleProject from "./SingleProject";
-import { motion } from "framer-motion";
-import { fadeIn } from "../../framerMotion/variants";
+import ProjectsText from "./ProjectsText";
 
 const projects = [
   {
-    name: "Netflix Clone",
-    year: "2022",
-    align: "right",
     image: "images/img1.jpg",
+    name: "Netflix Clone",
     link: "https://github.com/Rony-2004/Netflix-clone",
   },
   {
-    name: "Text to Speech Converter",
-    year: "2023",
-    align: "left",
     image: "images/t.png",
+    name: "Speech Converter",
     link: "https://github.com/Rony-2004/Speech-Converter",
   },
   {
-    name: "Tic Tac Toe",
-    year: "2023",
-    align: "right",
     image: "images/img3.jpg",
+    name: "Tic Tac Toe",
     link: "https://github.com/Rony-2004/Tic-Tac",
   },
   {
-    name: "Spotify Clone",
-    year: "2023",
-    align: "left",
     image: "images/img4.jpg",
+    name: "Portfolio",
     link: "https://github.com/Rony-2004/",
   },
   {
-    name: "Real Time Chat App",
-    year: "2024",
-    align: "right",
     image: "images/l.jpg",
+    name: "Lunatic AI",
     link: "https://github.com/Rony-2004/Lunatic-AI",
   },
   {
-    name: "Edu-Empower",
-    year: "2025",
-    align: "left",
     image: "images/5.png",
+    name: "Edu Empower",
     link: "https://edu-empower-rho.vercel.app/",
   },
   {
-    name: "Project-KS",
-    year: "2025",
-    align: "right",
     image: "images/7.png",
+    name: "Project KS",
     link: "https://github.com/Rony-2004/project-ks",
   },
   {
-    name: "Sim-Fluence",
-    year: "2025",
-    align: "left",
     image: "images/6.png",
+    name: "Simfluence",
     link: "https://simfluence.vercel.app/",
   },
 ];
 
 const ProjectsMain = () => {
   return (
-    <div id="projects" className="max-w-[1200px] mx-auto px-4">
-      <motion.div
-        variants={fadeIn("top", 0)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.7 }}
-      >
-        <ProjectsText />
-      </motion.div>
-      <div className="flex flex-col gap-20 max-w-[900px] mx-auto mt-12">
-        {projects.map((project, index) => {
-          return (
-            <SingleProject
-              key={index}
-              name={project.name}
-              year={project.year}
-              align={project.align}
-              image={project.image}
-            />
-          );
-        })}
+    <section id="projects" className="max-w-7xl mx-auto px-4">
+      <ProjectsText />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-12">
+        {projects.map((project, index) => (
+          <SingleProject
+            key={index}
+            image={project.image}
+            link={project.link}
+            name={project.name}
+          />
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
